1b0f515de48cfd8d363fc10323f341727f1ffccb
Moulgus <michal.jurewicz02@gmail.com>
2026-05-09 20:47:11 +0200

    Obsługa błędów 1

---
+++ 02 Web API/exercises/Zadanie 3/WeatherAPI/Data/CityStore.cs
@@ -0,0 +1,41 @@
+﻿using WeatherApi.Models;
+
+namespace WeatherApi.Data;
+
+public static class CityStore
+{
+    private static readonly List<City> _cities = new()
+    {
+        new City { ID = 1, Name = "Warsaw" },
+        new City { ID = 2, Name = "Krakow" }
+    };
+
+    private static int _nextID = 3;
+
+    public static IReadOnlyList<City> GetAll() => _cities;
+
+    public static City? GetByID(int id) => _cities.FirstOrDefault(c => c.ID == id);
+
+    public static City Add(string name)
+    {
+        var city = new City { ID = _nextID++, Name = name };
+        _cities.Add(city);
+        return city;
+    }
+
+    public static bool Delete(int id)
+    {
+        var city = GetByID(id);
+        if (city is null) return false;
+        _cities.Remove(city);
+        return true;
+    }
+
+    public static City? Update(int id, string name)
+    {
+        var city = GetByID(id);
+        if (city is null) return null;
+        city.Name = name;
+        return city;
+    }
+}
---
+++ 05 Wprowadzenie do obsługi błędów i logowania/Zad 1/Models/City.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherApi.Models;
+
+public sealed class City
+{
+    public int ID { get; set; }        
+    public string Name { get; set; } = "";
+}
