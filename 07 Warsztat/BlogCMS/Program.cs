2806485c5ded812c2e70c2412707938b9dd82699
Moulgus <michal.jurewicz02@gmail.com>
2026-06-07 16:05:52 +0200

    07 Warsztat

---
+++ 07 Warsztat/BlogCMS/Program.cs
@@ -0,0 +1,35 @@
+using BlogCMS.Data;
+using BlogCMS.Interfaces;
+using BlogCMS.Models;
+using BlogCMS.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddDbContext<BlogDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IRepository<Post>, EfCoreRepository<Post>>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
